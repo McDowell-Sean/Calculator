b0e854a7947a2615d3ce163d82914bcf7e3cc7aa
Sean <srmcdowell@student.fullsail.edu>
2026-06-12 22:11:57 -0500

    Created buttons inside the panel

--- SWE_App/Calculator.h
+++ SWE_App/Calculator.h
@@ -2,9 +2,45 @@
 
 #include <wx/wx.h>
 
-class Calculator : public wxFrame
+enum CalculatorButtonId	
 {
-public: 
-	Calculator();
+	ID_BUTTON_0 = wxID_HIGHEST + 1,
+	ID_BUTTON_1,
+	ID_BUTTON_2,
+	ID_BUTTON_3,
+	ID_BUTTON_4,
+	ID_BUTTON_5,
+	ID_BUTTON_6,
+	ID_BUTTON_7,
+	ID_BUTTON_8,
+	ID_BUTTON_9,
+
+	ID_BUTTON_ADD,
+	ID_BUTTON_SUBTRACT,
+	ID_BUTTON_MULTIPLY,
+	ID_BUTTON_DIVIDE,
+	ID_BUTTON_MODULO,
+
+	ID_BUTTON_SIN,
+	ID_BUTTON_COS,
+	ID_BUTTON_TAN,
+
+	ID_BUTTON_EQUALS,
+	ID_BUTTON_CLEAR,
+	ID_BUTTON_BACKSPACE,
+	ID_BUTTON_DECIMAL,
+	ID_BUTTON_NEGATIVE
 };
 
+
+
+
+class Calculator : public wxFrame
+{
+public:
+	Calculator();
+
+
+private:
+	wxTextCtrl* display;
+};
