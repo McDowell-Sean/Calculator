586b224754f03be71133522db308c7fbca1862e7
Sean <srmcdowell@student.fullsail.edu>
2026-06-18 00:43:58 -0500

    Final delete and finish

--- SWE_App/Calculator.cpp
+++ SWE_App/Calculator.cpp
@@ -88,45 +88,6 @@ Calculator::Calculator()
 	addButtonToGrid(ButtonFactory::CreateZeroButton(panel));
 	addButtonToGrid(ButtonFactory::CreateDecimalButton(panel));
 
-
-
-
-
-/*	auto addButton = [panel, buttonSizer](int id, const wxString& label)
-		{
-			wxButton* button = new wxButton(panel, id, label);
-
-			buttonSizer->Add(button, 0, wxEXPAND);
-		};
-
-	addButton(ID_BUTTON_SIN, "SIN");
-	addButton(ID_BUTTON_COS, "COS");
-	addButton(ID_BUTTON_TAN, "TAN");
-	addButton(ID_BUTTON_CLEAR, "C");
-	addButton(ID_BUTTON_BACKSPACE, "DEL");
-
-	addButton(ID_BUTTON_7, "7");
-	addButton(ID_BUTTON_8, "8");
-	addButton(ID_BUTTON_9, "9");
-	addButton(ID_BUTTON_DIVIDE, "/");
-	addButton(ID_BUTTON_MODULO, "%");
-
-	addButton(ID_BUTTON_4, "4");
-	addButton(ID_BUTTON_5, "5");
-	addButton(ID_BUTTON_6, "6");
-	addButton(ID_BUTTON_MULTIPLY, "*");
-	addButton(ID_BUTTON_SUBTRACT, "-");
-
-	addButton(ID_BUTTON_1, "1");
-	addButton(ID_BUTTON_2, "2");
-	addButton(ID_BUTTON_3, "3");
-	addButton(ID_BUTTON_ADD, "+");
-	addButton(ID_BUTTON_EQUALS, "=");
-
-	addButton(ID_BUTTON_NEGATIVE, "+/-");
-	addButton(ID_BUTTON_0, "0");
-	addButton(ID_BUTTON_DECIMAL, "."); //temp outcomment for testin
-*/
 	buttonSizer->AddSpacer(0);
 	buttonSizer->AddSpacer(0);
 	mainSizer->Add(buttonSizer, 1, wxEXPAND | wxLEFT | wxRIGHT | wxBOTTOM, 10);
